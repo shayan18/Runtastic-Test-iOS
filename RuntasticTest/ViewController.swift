a005c8b87a8ed9d9024993707d7427a1a1e11cd4
Shayan Ali <shayan.ali@veripark.com>
2019-09-15 21:29:58 +0500

    project setup

---
+++ RuntasticTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RuntasticTest
+//
+//  Created by Shayan Ali on 15/09/2019.
+//  Copyright © 2019 Shayan Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
